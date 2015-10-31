b88123484c71c2d9b899240822ea1d4c647427b8
veerakumarnice <veerakumarnice@gmail.com>
2015-10-31 11:28:39 +0530

    Swirl 6 lessons completed

---
+++ assignment3/best.R
@@ -0,0 +1,6 @@
+## returns a character vector with the name of the hospital
+## that has the best (i.e lowest) 30-day mortality for the 
+## speified outcome in that state
+best <- function(state, outcome) {
+  
+} 
